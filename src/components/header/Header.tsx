6df9eb37d684d6c2cbe25576b3119c348288bba6
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-04 09:41:24 +0200

    style: add screens sizes to tailwind

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import '../../index.css';
 
 function Header() {
-  return <h1 className="text-center bg-gray-400">Deportes Hoy</h1>;
+  return <h1 className="text-center">Deportes Hoy</h1>;
 }
 
 export default Header;
