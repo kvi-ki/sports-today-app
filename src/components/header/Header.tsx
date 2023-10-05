7157c95e59cb111f793061d30c672b8c40674d14
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-05 12:07:06 +0200

    style: add h3 style for Matches section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 function Header() {
   return (
-    <h1 className="text-center text-gray text-header p-2.5">Deportes Hoy</h1>
+    <h1 className="text-center text-gray text-header font-semibold p-4">
+      Deportes Hoy
+    </h1>
   );
 }
 
--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -3,7 +3,9 @@ import Match from '../match/Match';
 function Matches() {
   return (
     <div>
-      <h3>Hoy - Jueves, 5 de Octubre</h3>
+      <h3 className="text-center bg-gray text-white h-12 pt-2.5 font-medium">
+        Hoy - Jueves, 5 de Octubre
+      </h3>
       <div>
         <Match />
       </div>
