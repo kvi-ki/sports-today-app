6e30f920d644163d87b147c4f672c54d424cdb43
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-11 11:11:41 +0200

    test: should mock a match and doesn't call api

--- tests/sports.spec.tsx
+++ tests/sports.spec.tsx
@@ -4,16 +4,32 @@ test(`should mock a match and doesn't call api`, async ({ page }) => {
   await page.route('*/**/api/matches', async (route) => {
     const json = [
       {
-        matches: {
-          homeTeam: 'Real Madrid Fem.',
-          awayTeam: 'Torre del Mar'
-        }
+        day: '2023-10-12',
+        matches: [
+          {
+            startAt: '2023-10-12T16:00:00Z',
+            league: '',
+            homeTeam: 'Letonia',
+            homeTeamLogo:
+              'https://sportstves.b-cdn.net/teams/mini_407_201910120844.png',
+            awayTeam: 'Armenia',
+            awayTeamLogo:
+              'https://sportstves.b-cdn.net/teams/mini_219_20140427.png',
+            channels: [
+              {
+                name: 'UEFA TV',
+                paid: false
+              }
+            ]
+          }
+        ]
       }
     ];
+
     await route.fulfill({ json });
   });
 
   await page.goto('https://deportes-hoy.herokuapp.com/api/matches');
 
-  await expect(page.getByText('Real Madrid Fem.')).toBeVisible();
+  await expect(page.getByText('Letonia')).toBeVisible();
 });
