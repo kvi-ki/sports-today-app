3a9d7fd363a7eb33e67452295964b8715e662f87
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-19 17:40:19 +0200

    refact: move test to parallel testing

--- tests/sports.spec.tsx
+++ tests/sports.spec.tsx
@@ -1,39 +1,5 @@
 import { expect, test } from '@playwright/test';
 
-test(`should mock a match and doesn't call api`, async ({ page }) => {
-  await page.route('*/**/api/matches', async (route) => {
-    const json = [
-      {
-        day: '2023-10-12',
-        matches: [
-          {
-            startAt: '2023-10-12T16:00:00Z',
-            league: '',
-            homeTeam: 'Letonia',
-            homeTeamLogo:
-              'https://sportstves.b-cdn.net/teams/mini_407_201910120844.png',
-            awayTeam: 'Armenia',
-            awayTeamLogo:
-              'https://sportstves.b-cdn.net/teams/mini_219_20140427.png',
-            channels: [
-              {
-                name: 'UEFA TV',
-                paid: false
-              }
-            ]
-          }
-        ]
-      }
-    ];
-
-    await route.fulfill({ json });
-  });
-
-  await page.goto('https://deportes-hoy.herokuapp.com/api/matches');
-
-  await expect(page.getByText('Letonia')).toBeVisible();
-});
-
 test.describe.parallel('API testing', () => {
   const baseUrl = 'https://deportes-hoy.herokuapp.com/api';
 
@@ -47,5 +13,35 @@ test.describe.parallel('API testing', () => {
     const response = await request.get(`${baseUrl}/not-exosted-matches`);
 
     expect(response.status()).toBe(404);
+  });
+
+  test('should get match detail', async ({ page }) => {
+    await page.route('*/**/api/matches', async (route) => {
+      const json = {
+        day: '2023-10-12',
+        matches: {
+          startAt: '2023-10-12T16:00:00Z',
+          league: '',
+          homeTeam: 'Letonia',
+          homeTeamLogo:
+            'https://sportstves.b-cdn.net/teams/mini_407_201910120844.png',
+          awayTeam: 'Armenia',
+          awayTeamLogo:
+            'https://sportstves.b-cdn.net/teams/mini_219_20140427.png',
+          channels: [
+            {
+              name: 'UEFA TV',
+              paid: false
+            }
+          ]
+        }
+      };
+
+      await route.fulfill({ json });
+    });
+
+    await page.goto('https://deportes-hoy.herokuapp.com/api/matches');
+
+    await expect(page.getByText('Letonia')).toBeVisible();
   });
 });
