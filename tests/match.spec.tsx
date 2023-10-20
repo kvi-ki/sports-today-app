9266a6550fa6c72c5e488003eefde12eafb30445
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-20 10:02:43 +0200

    refact: remove unused test

--- tests/match.spec.tsx
+++ tests/match.spec.tsx
@@ -1,9 +1,5 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/');
-});
-
-test('should render homeTeam', async ({ page }) => {
-  await expect(page.getByTestId('homeTeam')).toHaveText('Letonia');
 });
