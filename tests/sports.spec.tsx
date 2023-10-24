4a219052a102b2ace94fc8d5ecd7ba75cd010762
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-24 12:58:30 +0200

    refactor: update json response

--- tests/sports.spec.tsx
+++ tests/sports.spec.tsx
@@ -1,43 +1,82 @@
 import { expect, test } from '@playwright/test';
 
-const apiResponse = {
-  day: '2023-10-12',
-  matches: [
-    {
-      startAt: '2023-10-12T16:00:00Z',
-      league: '',
-      homeTeam: 'Letonia',
-      homeTeamLogo:
-        'https://sportstves.b-cdn.net/teams/mini_407_201910120844.png',
-      awayTeam: 'Armenia',
-      awayTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_219_20140427.png',
-      channels: [
-        {
-          name: 'UEFA TV',
-          paid: false
-        }
-      ]
-    },
-    {
-      startAt: '2023-10-24T19:00:00Z',
-      league: '',
-      homeTeam: 'Sevilla',
-      homeTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_12_20130125.png',
-      awayTeam: 'Arsenal',
-      awayTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_126_20130210.png',
-      channels: [
-        {
-          name: 'M+ Liga de Campeones 2',
-          paid: true
-        },
-        {
-          name: 'M+ Liga de Campeones 4',
-          paid: true
-        }
-      ]
-    }
-  ]
-};
+const apiResponse = [
+  {
+    day: '2023-10-12',
+    matches: [
+      {
+        startAt: '2023-10-12T16:00:00Z',
+        league: '',
+        homeTeam: 'Letonia',
+        homeTeamLogo:
+          'https://sportstves.b-cdn.net/teams/mini_407_201910120844.png',
+        awayTeam: 'Armenia',
+        awayTeamLogo:
+          'https://sportstves.b-cdn.net/teams/mini_219_20140427.png',
+        channels: [
+          {
+            name: 'UEFA TV',
+            paid: false
+          }
+        ]
+      },
+      {
+        startAt: '2023-10-12T19:00:00Z',
+        league: '',
+        homeTeam: 'Sevilla',
+        homeTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_12_20130125.png',
+        awayTeam: 'Arsenal',
+        awayTeamLogo:
+          'https://sportstves.b-cdn.net/teams/mini_126_20130210.png',
+        channels: [
+          {
+            name: 'M+ Liga de Campeones 2',
+            paid: true
+          },
+          {
+            name: 'M+ Liga de Campeones 4',
+            paid: true
+          }
+        ]
+      }
+    ]
+  },
+  {
+    day: '2023-10-25',
+    matches: [
+      {
+        startAt: '2023-10-25T16:45:00Z',
+        league: '',
+        homeTeam: 'Feyenoord',
+        homeTeamLogo:
+          'https://sportstves.b-cdn.net/teams/mini_284_20140906.png',
+        awayTeam: 'Lazio',
+        awayTeamLogo:
+          'https://sportstves.b-cdn.net/teams/mini_167_20130919.png',
+        channels: [
+          {
+            name: 'M+ Liga de Campeones 3',
+            paid: true
+          }
+        ]
+      },
+      {
+        startAt: '2023-10-25T16:45:00Z',
+        league: '',
+        homeTeam: 'Barcelona',
+        homeTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_2_20130125.png',
+        awayTeam: 'Shaktar Donetsk',
+        awayTeamLogo: 'https://sportstves.b-cdn.net/teams/mini_0_20130207.png',
+        channels: [
+          {
+            name: 'M+ Liga de Campeones',
+            paid: true
+          }
+        ]
+      }
+    ]
+  }
+];
 
 test.describe.parallel('API testing', () => {
   const baseUrl = 'https://deportes-hoy.herokuapp.com/api';
