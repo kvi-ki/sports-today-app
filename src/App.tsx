e5a1cd11848c07cb70aa696718a951dc2d3632a3
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-24 12:30:48 +0200

    feat: use QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import Header from './components/header/Header';
 import Matches from './components/matches/Matches';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Matches />
-    </div>
+    </QueryClientProvider>
   );
 }
 
