8c57d5a054f18a4aea297472ccbc61a9362ac590
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-24 12:27:10 +0200

    feat: render homeTeams of matches

--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -1,12 +1,25 @@
-import Match from '../match/Match';
+import Match, { MatchProps } from '../match/Match';
+import { useMatches } from '../../api/sports';
 
 function Matches() {
+  const matches = useMatches();
+
+  const matchesList = () => {
+    if (Array.isArray(matches)) {
+      return (matches as MatchProps[]).map(
+        (match: MatchProps, index: number) => {
+          return <Match key={index} {...match} />;
+        }
+      );
+    } 
+  };
+
   return (
     <div>
       <h3 className="text-center bg-gray text-white h-12 pt-2.5 font-medium">
         Hoy - Jueves, 5 de Octubre
       </h3>
-      <Match />
+      <ul>{matchesList()}</ul>
     </div>
   );
 }
