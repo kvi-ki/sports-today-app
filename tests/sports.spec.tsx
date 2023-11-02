09d94c6fc0f00b81f63c19383fd49e526d60d879
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-02 14:55:43 +0100

    refactor: remove unused test

--- tests/sports.spec.tsx
+++ tests/sports.spec.tsx
@@ -1,8 +1,4 @@
 import { expect, test } from '@playwright/test';
-import { renderHook, waitFor } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
-import { useMatches } from './api.mock';
 
 const baseUrl = 'https://deportes-hoy.herokuapp.com/api';
 
@@ -16,15 +12,4 @@ test('should assert invalid endpoint', async ({ request }) => {
   const response = await request.get(`${baseUrl}/not-exosted-matches`);
 
   expect(response.status()).toBe(404);
-});
-
-test.only('should get matches', async () => {
-  const queryClient = new QueryClient();
-  const wrapper = ({ children }) => (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-
-  const { result } = renderHook(() => useMatches(), { wrapper });
-
-  await waitFor(() => expect(result.current).toBe(true));
 });
