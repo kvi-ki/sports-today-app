191615b32eda9ba3aac9b4e7619007cde74faca4
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-03 10:04:33 +0100

    refactor: extract response mock to beforeEach

--- tests/match.spec.tsx
+++ tests/match.spec.tsx
@@ -1,11 +1,7 @@
 import { expect, test } from '@playwright/test';
 import responseContent from './response.json' assert { type: 'json' };
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto('http://localhost:5173/');
-// });
-
-test('should print home teams list', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.route('*/**/api/matches', (route) => {
     route.fulfill({
       status: 200,
@@ -15,23 +11,15 @@ test('should print home teams list', async ({ page }) => {
   });
 
   await page.goto('/');
+});
 
+test('should print home teams list', async ({ page }) => {
   await expect(page.getByText('Letonia')).toBeVisible();
   await expect(page.getByText('Feyenoord')).toBeVisible();
   await expect(page.getByRole('listitem')).toHaveCount(4);
 });
 
 test('should print away teams list', async ({ page }) => {
-  await page.route('*/**/api/matches', (route) => {
-    route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      json: responseContent
-    });
-  });
-
-  await page.goto('/');
-
   await expect(page.getByText('Armenia')).toBeVisible();
   await expect(page.getByText('Lazio')).toBeVisible();
   await expect(page.getByRole('listitem')).toHaveCount(4);
