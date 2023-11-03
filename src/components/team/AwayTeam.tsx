2c8b2d8d5c9b3fcc6b8362d5235fae154c1b9a4e
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-03 11:36:58 +0100

    style: add flex for team renders

--- src/components/team/AwayTeam.tsx
+++ src/components/team/AwayTeam.tsx
@@ -2,7 +2,7 @@ import { TeamProps } from './Team';
 
 function AwayTeam(team: TeamProps) {
   return (
-    <div>
+    <div className="flex justify-between">
       <h2 className="text-gray pt-3.5">{team.teamName}</h2>
       <img src={team.teamLogo} alt="team logo" />
     </div>
