9a14cb014c99d6d99df65e9c79f82bd54cf35c3e
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-03 09:49:09 +0100

    test: should print away teams list

--- tests/match.spec.tsx
+++ tests/match.spec.tsx
@@ -1,5 +1,38 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+import responseContent from './response.json' assert { type: 'json' };
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+// test.beforeEach(async ({ page }) => {
+//   await page.goto('http://localhost:5173/');
+// });
+
+test('should print home teams list', async ({ page }) => {
+  await page.route('*/**/api/matches', (route) => {
+    route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      json: responseContent
+    });
+  });
+
+  await page.goto('/');
+
+  await expect(page.getByText('Letonia')).toBeVisible();
+  await expect(page.getByText('Feyenoord')).toBeVisible();
+  await expect(page.getByRole('listitem')).toHaveCount(4);
+});
+
+test('should print away teams list', async ({ page }) => {
+  await page.route('*/**/api/matches', (route) => {
+    route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      json: responseContent
+    });
+  });
+
+  await page.goto('/');
+
+  await expect(page.getByText('Armenia')).toBeVisible();
+  await expect(page.getByText('Lazio')).toBeVisible();
+  await expect(page.getByRole('listitem')).toHaveCount(4);
 });
