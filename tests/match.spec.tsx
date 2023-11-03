f0b2d8a66c05340017cf9b909a1de5534a18e18a
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-03 11:26:50 +0100

    test: should print teams banner

--- tests/match.spec.tsx
+++ tests/match.spec.tsx
@@ -24,3 +24,8 @@ test('should print away teams list', async ({ page }) => {
   await expect(page.getByText('Lazio')).toBeVisible();
   await expect(page.getByRole('listitem')).toHaveCount(4);
 });
+
+test('should print teams banner', async ({ page }) => {
+  await expect(page.getByAltText('team logo')).toBeVisible();
+  await expect(page.getByAltText('team logo')).toHaveCount(8);
+});
