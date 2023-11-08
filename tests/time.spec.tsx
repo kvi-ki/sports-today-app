40ccc6c38e3543ab7c9692ef8466d68c4e0bd70b
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-08 10:06:39 +0100

    test: add new assertion to should print time of matches

--- tests/time.spec.tsx
+++ tests/time.spec.tsx
@@ -14,7 +14,8 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('should print time of matches', async ({ page }) => {
-  await expect(page.getByText(/\d{2}:\d{2}/)).toHaveCount(4);
   await expect(page.getByText('16:00')).toBeVisible();
   await expect(page.getByText('16:45').first()).toBeVisible();
+  await expect(page.getByText(/\d{2}:\d{2}/)).toHaveCount(4);
+  expect('19:00').toEqual(expect.stringMatching(/\d{2}:\d{2}/));
 });
