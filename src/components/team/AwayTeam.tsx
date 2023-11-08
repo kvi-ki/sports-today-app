bd4a331a1d0077f1405b21a73a375ab26e8710e8
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-08 12:37:20 +0100

    style: update team font and flex

--- src/components/team/AwayTeam.tsx
+++ src/components/team/AwayTeam.tsx
@@ -2,8 +2,8 @@ import { TeamProps } from './Team';
 
 function AwayTeam(team: TeamProps) {
   return (
-    <div className="flex justify-around">
-      <h2 className="text-gray pt-3.5">{team.teamName}</h2>
+    <div className="flex justify-evenly">
+      <h2 className="text-gray text-team pt-3.5">{team.teamName}</h2>
       <img src={team.teamLogo} alt="team logo" />
     </div>
   );
--- src/components/team/HomeTeam.tsx
+++ src/components/team/HomeTeam.tsx
@@ -2,9 +2,9 @@ import { TeamProps } from './Team';
 
 function HomeTeam(team: TeamProps) {
   return (
-    <div className="flex justify-around">
+    <div className="flex justify-evenly">
       <img src={team.teamLogo} alt="team logo" />
-      <h2 className="text-gray pt-3.5">{team.teamName}</h2>
+      <h2 className="text-gray text-team pt-3.5">{team.teamName}</h2>
     </div>
   );
 }
