6c0e63471160dbab0073e87fad2fd27826a60040
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-10 11:08:40 +0100

    refactor: simplify condition for not paid channel

--- src/components/channels/Channel.tsx
+++ src/components/channels/Channel.tsx
@@ -4,23 +4,17 @@ export type ChannelProps = {
 };
 
 export default function Channel(channel: ChannelProps) {
-  if (channel.paid === false) {
-    return (
-      <li
-        data-testid="channel"
-        className="bg-green rounded-lg text-center text-white text-channel w-28 p-1"
-      >
-        {channel.name}
-      </li>
-    );
-  } else {
-    return (
-      <li
-        data-testid="channel"
-        className="bg-navy rounded-lg text-center text-white text-channel w-28 p-1"
-      >
-        {channel.name}
-      </li>
-    );
-  }
+  const classChoose = () => {
+    if (channel.paid === false) {
+      return 'className=rounded-lg text-center text-white text-channel w-28 p-1 rounded bg-green';
+    } else {
+      return 'className=rounded-lg text-center text-white text-channel w-28 p-1 rounded bg-navy';
+    }
+  };
+
+  return (
+    <li data-testid="channel" className={classChoose()}>
+      {channel.name}
+    </li>
+  );
 }
