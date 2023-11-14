9ad7cde3e2ec92bb7f94722954331208e941425b
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-14 15:41:33 +0100

    style: change header colour and add shadow

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <h1
       id="header"
-      className="text-center text-gray text-header font-semibold p-4"
+      className="text-center text-green drop-shadow-[1px_2px_2px_rgba(128,93,83,50)] text-header font-semibold p-4"
     >
       Deportes Hoy
     </h1>
