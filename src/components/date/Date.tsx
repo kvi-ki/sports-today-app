24647fc7143018346e0fb161d1eab5b92cd15c84
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-17 10:53:17 +0100

    refactor: remove unused style

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -6,7 +6,7 @@ export default function Date(date: DateProps) {
   return (
     <p
       data-testid="date"
-      className="w-11/12 text-center text-navy font-bold h-12 pt-2.5 text-date"
+      className="text-center text-navy font-bold pt-2.5 text-date"
     >
       {date.day}
     </p>
