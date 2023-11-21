d2d532ba2befe85d4f2f5060cbc142061b15c4ca
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-21 12:33:50 +0100

    refactor: change to ternary for today date condition

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -6,23 +6,12 @@ export default function DateMatch(date: DateProps) {
   const todayJson = new Date().toJSON();
   const today = todayJson.slice(0, 10);
 
-  if (date.day === today) {
-    return (
-      <p
-        data-testid="date"
-        className="hurrey text-center text-navy font-bold pt-2.5 text-date"
-      >
-        Hoy
-      </p>
-    );
-  } else {
-    return (
-      <p
-        data-testid="date"
-        className="text-center text-navy font-bold pt-2.5 text-date"
-      >
-        {date.day}
-      </p>
-    );
-  }
+  return (
+    <p
+      data-testid="date"
+      className="text-center text-navy font-bold pt-2.5 text-date"
+    >
+      {date.day === today ? 'Hoy' : date.day}
+    </p>
+  );
 }
