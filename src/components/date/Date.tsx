4b833c40b1261b90853311eef97d20ac9ca75937
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-21 12:31:33 +0100

    feat: add condition for today date appearence

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -2,13 +2,27 @@ type DateProps = {
   day: string;
 };
 
-export default function Date(date: DateProps) {
-  return (
-    <p
-      data-testid="date"
-      className="text-center text-navy font-bold pt-2.5 text-date"
-    >
-      {date.day}
-    </p>
-  );
+export default function DateMatch(date: DateProps) {
+  const todayJson = new Date().toJSON();
+  const today = todayJson.slice(0, 10);
+
+  if (date.day === today) {
+    return (
+      <p
+        data-testid="date"
+        className="hurrey text-center text-navy font-bold pt-2.5 text-date"
+      >
+        Hoy
+      </p>
+    );
+  } else {
+    return (
+      <p
+        data-testid="date"
+        className="text-center text-navy font-bold pt-2.5 text-date"
+      >
+        {date.day}
+      </p>
+    );
+  }
 }
