02ab5d4839c99bc927e1f679f09017786e5d5581
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-11-27 15:55:59 +0100

    test: change the web link

--- tests/header.spec.tsx
+++ tests/header.spec.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('https://musical-pasca-16c8b2.netlify.app/');
 });
 
 test('should render header', async ({ page }) => {
