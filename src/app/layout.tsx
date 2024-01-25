662e878da9c85fbf08784f488b97b25c6efe5a0f
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-01-25 14:17:38 +0100

    feat: add meta with description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="description" content="Consulta los partidos de fútbol de hoy en televisión. Consulta horarios y canales dónde ver el futbol por la TV"></meta>
       <body>
         <div id="root">{children}</div>
       </body>
