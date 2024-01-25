df465f7c96e7d18ab407ad89c1c0e3b6b7247e33
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-01-25 14:02:30 +0100

    feat: create homeTeam and awayTeam imstead of Team

--- src/components/match/Match.tsx
+++ src/components/match/Match.tsx
@@ -2,6 +2,8 @@ import Time from '../time/Time';
 import Channels from '../channels/Channels';
 import { ChannelProps } from '../channels/Channel';
 import Team from '../team/Team';
+import HomeTeam from '../team/HomeTeam';
+import AwayTeam from '../team/AwayTeam';
 
 export type MatchProps = {
   startAt: string;
@@ -14,14 +16,14 @@ export type MatchProps = {
 
 export default function Match(match: MatchProps) {
   return (
-    <li className="m-3.5 pt-3 w-80 rounded-sm shadow-2xl bg-gray grid grid-flow-row grid-cols-3 auto-rows-min gap-1 place-items-center md:w-85">
-      <Team
+    <li className="m-3.5 pt-2 w-11/12 rounded-md bg-gray grid grid-flow-row grid-cols-5 auto-rows-min gap-1 place-items-center md:w-4/5 rounded-lg xl:w-2/5">
+      <HomeTeam
         teamName={match.homeTeam}
         teamLogo={match.homeTeamLogo}
         data-testid="homeTeam"
       />
       <Time startAt={match.startAt} />
-      <Team
+      <AwayTeam
         teamName={match.awayTeam}
         teamLogo={match.awayTeamLogo}
         data-testid="awayTeam"
---
+++ src/components/team/HomeTeam.tsx
@@ -0,0 +1,16 @@
+import { TeamProps } from './Team';
+
+export default function HomeTeam(team: TeamProps) {
+  return (
+    <div className="col-span-2 gap-4 flex flex-row items-center lg:gap-6">
+      <img
+        src={team.teamLogo}
+        className="w-7 h-8 md:w-9 h-10 lg:w-10 h-full"
+        alt="team logo"
+      />
+      <h2 className="text-navy text-team text-center md:text-teamMd">
+        {team.teamName}
+      </h2>
+    </div>
+  );
+}
