0ea777dad665280d6ed21f44921b3b57e00b167d
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-01-29 11:39:35 +0100

    style: update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,9 @@ export default function Header() {
   return (
     <h1
       id="header"
-      className="text-center bg-gradient-to-r from-brightOrange to-bgOrange text-white text-header font-semibold p-2 lg:p-3 text-headerLg"
+      className="border-b border-navy text-center text-orange text-header font-semibold p-2 lg:p-3 text-headerLg"
     >
-      Deportes Hoy
+      DeportesHoy
     </h1>
   );
 }
