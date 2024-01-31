bcb537dc8da1bcbb89f7818b8a2ec45f70077ed0
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-01-31 12:28:53 +0100

    feat: use local time zone

--- src/components/time/Time.tsx
+++ src/components/time/Time.tsx
@@ -1,15 +1,23 @@
+'use client';
+
+import { Suspense } from 'react';
+import { useHydration } from '../../hooks/useHydration';
+
 type TimeProps = {
   startAt: string;
 };
 
 export default function Time(time: TimeProps) {
-  const timeFormated = () => {
-    return time.startAt.slice(11, 16);
-  };
+  const hydrated = useHydration();
 
   return (
     <div className="col-span-1 row-span-2 place-self-center text-orange p-1.5 rounded-md font-bold text-time lg:text-timeLg">
-      {timeFormated()}
+      <Suspense key={hydrated ? 'local' : 'utc'}>
+        <time dateTime={time.startAt}>
+          {new Date(time.startAt).toLocaleTimeString().slice(0, 5)}
+          {hydrated ? '' : ' (UTC)'}
+        </time>
+      </Suspense>
     </div>
   );
 }
