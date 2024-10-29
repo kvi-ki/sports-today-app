1ecee95bf8dc067b7afd4d1b543b40d93d43f8e8
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-10-29 13:35:36 +0100

    feat: correct the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 export default function Header() {
   return (
     <h1
       id="header"
       className="border-b border-navy text-center text-orange text-header font-semibold p-2 lg:p-3 text-headerLg"
     >
-      DeportesHoy
+      Deportes Hoy
     </h1>
   );
 }
