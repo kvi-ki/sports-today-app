3e1e8d6b1ae186dd978cf91e395e1fe77a2b0210
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-11-11 12:48:41 +0100

    test: should render a heading

---
+++ __tests__/header.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Header from '../src/components/header/Header';
+import React from 'react';
+
+describe('Header', () => {
+  it('should render a heading', () => {
+    render(<Header />);
+
+    const heading = screen.getByRole('heading', { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
