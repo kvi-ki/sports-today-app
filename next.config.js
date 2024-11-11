064533516369646986c186868f86842ae7505e66
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-11-11 12:42:48 +0100

    chore: update export in config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    // distDir: './dist', 
-  }
-   
-  export default nextConfig
+  // output: 'export',
+  // distDir: './dist', 
+  experimental: {
+    testProxy: true,
+  },
+}
+
+module.exports = nextConfig
