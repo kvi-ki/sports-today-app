fc5e9373e6ccfb35764bb79783d59df8bc9e0954
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-11-13 09:19:26 +0100

    feat: use try and catch for fetching data

--- src/api/sports.tsx
+++ src/api/sports.tsx
@@ -1,12 +1,16 @@
-export async function getMatches() {
-  const url = process.env.API_URL + '/api/matches';
-  const response: Response = await fetch(url, {
-    next: { revalidate: 60 }
-  });
+export async function getData() {
+  try {
+    const url = process.env.NEXT_PUBLIC_API_URL + '/api/matches';
+    const response: Response = await fetch(url, {
+      next: { revalidate: 60 }
+    });
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch data');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Failed to fetch: ', error);
   }
-
-  return response.json();
 }
