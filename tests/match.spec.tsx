b586245dcd73d6e2f60342248f607bc6afbac9f9
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-11-14 12:39:18 +0100

    test: fix the api response mock

--- tests/match.spec.tsx
+++ tests/match.spec.tsx
@@ -1,18 +1,20 @@
-import { test, expect } from 'next/experimental/testmode/playwright';
+import { expect, test } from '@playwright/test';
 import responseContent from './response.json';
 
 test.describe('Hello', () => {
-  test.beforeEach(async ({ page, next }) => {
-    next.onFetch((request) => {
-      if (request.url.includes('/api/matches')) {
-        return new Response(JSON.stringify(responseContent));
-      }
+  test.beforeEach(async ({ page }) => {
+    await page.route('*/**/api/matches', (route) => {
+      route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        json: responseContent
+      });
     });
 
     await page.goto('/');
   });
 
-  test.only('should print home teams list', async ({ page }) => {
+  test('should print home teams list', async ({ page }) => {
     await expect(page.getByText('Letonia')).toBeVisible();
     await expect(page.getByText('Feyenoord')).toBeVisible();
     //await expect(page.getByTestId('homeTeam')).toHaveCount(4);
